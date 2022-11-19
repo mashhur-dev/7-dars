634bf032d9837bb83bd56eb018505121c338d151
mashhur-dev <mashhurtadjiyev@gmail.com>
2022-11-19 21:36:38 +0500

    7-dars

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+//  function sum(a: number, b: number): string {
+//     return (a + b).toString()
+//  }
+
+//  let addition: (a: number, b: number) => string = sum
+
+//  let s: string = "Result, " + addition(13, 2)
+
+//  console.log(s);
+ 
+//  function process_() {
+//     console.log(`Start process: Download Java - Overdoe.mp3 | 4.25Mb`);
+//     console.log('1%');
+//     console.log('14%');
+//     console.log('18%');
+//     console.log('21%');
+//     console.log('25%');
+//     return "result"
+//     console.log('31%');
+//     console.log('36%');
+//     console.log('39%');
+//     return
+//  }
+
+//  let res: string | undefined = process_()
+
+//  if (typeof res == "string") {
+//     console.log(res[0]);
+//  }
+// else {
+//     console.log(res);
+    
+// }
